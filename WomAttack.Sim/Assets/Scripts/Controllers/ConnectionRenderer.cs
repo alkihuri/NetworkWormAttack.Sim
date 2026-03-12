54ec9b77723aa5ed2e55f71bdf7c0bd5d3db7b82
alkihuri <alkihuri@gmail.com>
2026-03-12 23:24:28 +0100

    vibe code 2

--- WomAttack.Sim/Assets/Scripts/Controllers/ConnectionRenderer.cs
+++ WomAttack.Sim/Assets/Scripts/Controllers/ConnectionRenderer.cs
@@ -6,21 +6,51 @@ public class ConnectionRenderer : MonoBehaviour
     private const float LINE_WIDTH = 0.05f;
 
     private LineRenderer lineRenderer;
+    private Transform startTransform;
+    private Transform endTransform;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
 
     public void Initialize(Transform startTransform, Transform endTransform)
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        this.startTransform = startTransform;
+        this.endTransform = endTransform;
+
+        if (lineRenderer == null)
+            lineRenderer = GetComponent<LineRenderer>();
+
         lineRenderer.positionCount = 2;
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.loop = false;
         lineRenderer.startWidth = LINE_WIDTH;
         lineRenderer.endWidth = LINE_WIDTH;
 
         var shader = Shader.Find("Sprites/Default");
         if (shader == null)
             shader = Shader.Find("Standard");
-        lineRenderer.material = new Material(shader);
+
+        if (lineRenderer.material == null)
+            lineRenderer.material = new Material(shader);
+
         lineRenderer.startColor = Color.white;
         lineRenderer.endColor = Color.white;
 
+        UpdateLinePositions();
+    }
+
+    private void LateUpdate()
+    {
+        if (startTransform == null || endTransform == null || lineRenderer == null)
+            return;
+
+        UpdateLinePositions();
+    }
+
+    private void UpdateLinePositions()
+    {
         lineRenderer.SetPosition(0, startTransform.position);
         lineRenderer.SetPosition(1, endTransform.position);
     }
